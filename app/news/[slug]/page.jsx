1d4b7bc1ca1f32d35fafd22eb5207001aa3b628d
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-07-31 09:41:31 -0300

    Using dummy data to fill content

---
+++ app/news/[slug]/page.jsx
@@ -0,0 +1,17 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsDetailPage({ params }) {
+  const { slug } = params;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
+
+  return (
+    <article className="news-article">
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>News details page</h1>
+        <time datetime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>News Slug: {slug}</p>
+    </article>
+  );
+}
