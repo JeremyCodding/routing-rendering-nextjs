4c3ecdf7f8f7dcf5cb6fc26affac748ec42549cc
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-01 09:52:28 -0300

    Adding parallel routes with intercepting routes for different rendering

--- app/news/[slug]/@modal/(.)image/page.jsx
+++ app/news/[slug]/@modal/(.)image/page.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
@@ -12,10 +16,12 @@ export default function InterceptedImagePage({ params }) {
   }
   return (
     <>
-      <h2>Intercepted</h2>
-      <div className="fullscreen-image">
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-      </div>
+      <div className="modal-backdrop" onClick={router.back} />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
     </>
   );
 }
