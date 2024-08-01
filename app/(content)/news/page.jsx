74a94c40ae90a8c8015cb6bc1b126594983f8644
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-01 13:07:30 -0300

    Adding a backend in the project and starting with server side fetching

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,22 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+  const news = await response.json();
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
